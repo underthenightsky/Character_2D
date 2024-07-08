fd6066d4a0e6795fe906cb05d28fbbc19fe4aa7b
josephkurianjohn49 <128809455+underthenightsky@users.noreply.github.com>
2024-07-08 20:29:21 +0530

    update

--- App.js
+++ App.js
@@ -17,12 +17,12 @@ export default class App extends React.Component {
       <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
         <SafeAreaView style={{ flex: 1 }}>
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <SpriteSheet
+            {/* <SpriteSheet
               ref={ref => (this.mummy = ref)}
               source={require('./public/sprite/spritesheet.png')}
               columns={18}
               rows={18}
-              // height={200} // set either, none, but not both
+              height={700} // set either, none, but not both
               // width={200}
               imageStyle={{ marginTop: -1 }}
               animations={{
@@ -37,6 +37,27 @@ export default class App extends React.Component {
               <Button onPress={() => this.play('idle')} title="idle" />
               <Button onPress={() => this.play('smiling')} title="smiling" />
               <Button onPress={() => this.play('pointing')} title="pointing" />
+              <Button onPress={this.stop} title="stop" /> */}
+              <SpriteSheet
+              ref={ref => (this.mummy = ref)}
+              source={require('./public/sprite/mummy.png')}
+              columns={9}
+              rows={6}
+              // height={200} // set either, none, but not both
+              // width={200}
+              imageStyle={{ marginTop: -1 }}
+              animations={{
+                walk: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                appear: Array.from({ length: 15 }, (v, i) => i + 18),
+                die: Array.from({ length: 21 }, (v, i) => i + 33)
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 30, paddingHorizontal: 30 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+              <Button onPress={() => this.play('walk')} title="walk" />
+              <Button onPress={() => this.play('appear')} title="appear" />
+              <Button onPress={() => this.play('die')} title="die" />
               <Button onPress={this.stop} title="stop" />
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -49,11 +70,11 @@ export default class App extends React.Component {
               />
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={{ fontSize: 16, marginRight: 10,color:'white' }}>Loop</Text>
+              <Text style={{ fontSize: 16, marginRight: 10,color:'blue' }}>Loop</Text>
               <Switch value={loop} onValueChange={loop => this.setState({ loop })} />
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={{ fontSize: 16, marginRight: 10,color:'white' }}>Reset After Finish</Text>
+              <Text style={{ fontSize: 16, marginRight: 10,color:'blue' }}>Reset After Finish</Text>
               <Switch
                 value={resetAfterFinish}
                 onValueChange={val => this.setState({ resetAfterFinish: val })}
