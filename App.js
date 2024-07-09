433eda951860f4d6da1dd5d29f2eef63b9f686e5
josephkurianjohn49 <128809455+underthenightsky@users.noreply.github.com>
2024-07-09 20:13:19 +0530

    updated_2d_avatar

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import {TextInput,Text, View, Button,Switch , KeyboardAvoidingView ,SafeAreaView} from 'react-native';
 import  {SpriteSheet}  from './scripts/spriteSheet';
+import Sound from 'react-native-sound';
 
 
+
+var sound = new Sound('jithu_intro_combined.mp3');
+const playSound = () => {
+  sound.play();   
+ };
 export default class App extends React.Component {
   state = {
     loop: false,
     resetAfterFinish: false,
-    fps: '16'
+    fps: '16',
   };
 
-  render() {
-    const { fps, loop, resetAfterFinish } = this.state;
+setAnimation =() => {this.play('total')};
 
+ componentDidMount(){
+  // playSound();  
+  this.setAnimation();  
+  
+ }
+
+
+
+  render() {
+    const { fps, loop, resetAfterFinish,sound } = this.state;
+    var whoosh = new Sound('jithu_intro_merged.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+    
+      // Play the sound with an onEnd callback
+      whoosh.play((success) => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+        }
+      });
+    });
     return (
       <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
         <SafeAreaView style={{ flex: 1 }}>
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            {/* <SpriteSheet
+            <SpriteSheet
               ref={ref => (this.mummy = ref)}
               source={require('./public/sprite/spritesheet.png')}
               columns={18}
@@ -26,39 +58,21 @@ export default class App extends React.Component {
               // width={200}
               imageStyle={{ marginTop: -1 }}
               animations={{
-                idle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-                smiling: Array.from({ length: 170 }, (v, i) => i + 18),
-                pointing: Array.from({ length: 136 }, (v, i) => i + 188)
+                // idle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                // smiling: Array.from({ length: 170 }, (v, i) => i + 18),
+                // pointing: Array.from({ length: 136 }, (v, i) => i + 188),
+                total : Array.from({ length: 320 }, (v, i) => i + 0),
+                idle : Array.from({ length: 10 }, (v, i) => i + 0),
               }}
             />
           </View>
-          <View style={{ paddingVertical: 30, paddingHorizontal: 30 }}>
+          {/* <View style={{ paddingVertical: 30, paddingHorizontal: 30 }}>
             <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
               <Button onPress={() => this.play('idle')} title="idle" />
               <Button onPress={() => this.play('smiling')} title="smiling" />
               <Button onPress={() => this.play('pointing')} title="pointing" />
-              <Button onPress={this.stop} title="stop" /> */}
-              <SpriteSheet
-              ref={ref => (this.mummy = ref)}
-              source={require('./public/sprite/mummy.png')}
-              columns={9}
-              rows={6}
-              // height={200} // set either, none, but not both
-              // width={200}
-              imageStyle={{ marginTop: -1 }}
-              animations={{
-                walk: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-                appear: Array.from({ length: 15 }, (v, i) => i + 18),
-                die: Array.from({ length: 21 }, (v, i) => i + 33)
-              }}
-            />
-          </View>
-          <View style={{ paddingVertical: 30, paddingHorizontal: 30 }}>
-            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-              <Button onPress={() => this.play('walk')} title="walk" />
-              <Button onPress={() => this.play('appear')} title="appear" />
-              <Button onPress={() => this.play('die')} title="die" />
               <Button onPress={this.stop} title="stop" />
+           
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
               <Text style={{ fontSize: 16, marginRight: 10 }}>FPS</Text>
@@ -78,9 +92,9 @@ export default class App extends React.Component {
               <Switch
                 value={resetAfterFinish}
                 onValueChange={val => this.setState({ resetAfterFinish: val })}
-              />
-            </View>
-          </View>
+              /> */}
+            {/* </View>
+          </View> */}
         </SafeAreaView>
       </KeyboardAvoidingView>
     );
